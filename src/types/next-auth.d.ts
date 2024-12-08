07e2a48c866c767b2e31bac627dfbe4abecabbe5
MTRFYMD <mtrfymd@gmail.com>
2024-12-08 21:58:35 +0900

    no message

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,6 +1,6 @@
 // frontend/src/types/next-auth.d.ts
 
-import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
+import { DefaultUser } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
